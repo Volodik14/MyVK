658e0175cc71843c7915898d9adb6e8913f31cb9
Motorkin Vladimir <vmmotorkin@edu.hse.ru>
2022-01-31 17:25:56 +0300

    Initial Commit
    1/8 done.

---
+++ MyVK/LoginFormController.swift
@@ -0,0 +1,90 @@
+//
+//  LoginFormController.swift
+//  MyVK
+//
+//  Created by Владимир Моторкин on 31.01.2022.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var loginInput: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        // получаем текст логина
+        let login = loginInput.text!
+        // получаем текст пароль
+        let password = passwordInput.text!
+                
+        // проверяем верны ли они
+        if login == "admin" && password == "228758" {
+            print("успешная авторизация")
+        } else {
+            print("неуспешная авторизация")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // жест нажатия
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(self.hideKeyboard))
+        // присваиваем его UIScrollVIew
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+        // получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        // добавляем отступ внизу UIScrollView равный размеру клавиатуры
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+        }
+        
+    //когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+            // устанавливаем отступ внизу UIScrollView равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Подписываемся на два уведомления, одно приходит при появлении клавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+        // Второе когда она пропадает
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+            
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func hideKeyboard() {
+            self.scrollView?.endEditing(true)
+        }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
